fb17ac77d8d741313d4315671c472385658d2139
unknown <victordsgnr@gmail.com>
2020-01-08 22:56:20 -0300

    Carreção no tratamento de error no metodo de login

--- app/Controllers/Http/LoginController.js
+++ app/Controllers/Http/LoginController.js
@@ -2,17 +2,32 @@
 const User = use('App/models/User')
 const Hash = use('Hash')
 
+class CustomError extends Error {
+  constructor(error) {
+    super(error)
+    this.message = error.message
+    this.error = error.error
+  }
+}
+
 class LoginController {
   async singIn({ auth, request, response }) {
     try {
       let { email, password } = request.body
-      let { token } = await auth.attempt(email, password, { email, password })
 
-      return response.send({ token })
+      let user = await User.findBy('email', email)
+
+      try {
+        let { token } = await auth.attempt(email, password, { user })
+        return response.send({ token })
+      } catch (err) {
+        throw new CustomError({ error: "NOT_FOUND", message: "Usuário não encontrado" })
+      }
+
     } catch (err) {
-      if (err.message == "E_PASSWORD_MISMATCH: Cannot verify user password")
-        return response.status(404).send({ message: "Usuario não encontrado" })
-      response.status(500).send({ message: err.message, stack: err.stack })
+      if (err.error == 'NOT_FOUND')
+        return response.status(404).send({ message: err.message })
+      return response.status(500).send({ message: err.message, stack: err.stack })
     }
   }
 }
