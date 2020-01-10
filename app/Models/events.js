1bc89d4952b8a2621e6577669ba07dfbccf32527
Camara <vlessa.terceiro@brasilcap.com.br>
2020-01-10 16:44:21 -0300

    cirando associação com days week no model events

--- app/Models/events.js
+++ app/Models/events.js
@@ -1,17 +1,24 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Events = sequelize.define('Events', {
-    id: DataTypes.INTEGER,
+  const Events = sequelize.define('events', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    day_week: {
+      type: DataTypes.INTEGER
     },
     description: {
       type: DataTypes.STRING
     } 
   }, {});
   Events.associate = function(models) {
-    // associations can be defined here
+    Events.belongsTo(models.days_week, { as: 'days_week' })
   };
   return Events;
 };
